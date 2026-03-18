9042f06d44f6fb7923d8c723e8519b4bdd2de892
ShamsAziz03 <shamsimad550@gmail.com>
2026-03-18 10:43:46 +0200

    reactor: add circle raduis to comparing with new circle in undo

--- circle-drawer/script.js
+++ circle-drawer/script.js
@@ -107,7 +107,8 @@ function undo() {
     for (let i = 0; i < elements.circles.length; i++) {
       if (
         elements.circles[i].x === elements.lastChange.newCircle.x &&
-        elements.circles[i].y === elements.lastChange.newCircle.y
+        elements.circles[i].y === elements.lastChange.newCircle.y &&
+        elements.circles[i].r === elements.lastChange.newCircle.r
       ) {
         elements.circles[i].r = elements.lastChange.oldCircle.r;
         redrawCircles();
